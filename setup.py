b0dc2a8182614aaa2969c32ce4ec629be6335746
Colin Powell <powellc@rydberg.local>
2010-01-31 22:30:40 -0500

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='django-contact-form',
+      version='0.1',
+      description='Generic massage application for Django',
+      author='Colin Powell',
+      author_email='colin.powell@me.com',
+      url='http://http://github.com/powellc/django-massages/',
+      packages=['massages'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
+
